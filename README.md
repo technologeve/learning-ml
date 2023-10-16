6f4a9cc151a93669e585df3de2823b721eee25ce
technologeve <eve.nicole@icloud.com>
2023-10-16 09:05:10 +0100

    Initial wine classification (0.94 accuracy)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # learning-ml
-Completing selected challenges from Microsoft Learn's 'Create machine learning models' pathway
+
+Completing selected challenges from Microsoft Learn's 'Create machine learning models' pathway, to familiarise myself with applying common machine learning alogrithms.
---
+++ classification_models/classify_wine.py
@@ -0,0 +1,52 @@
+""" Completing Microsoft Learn"s Wine Classification Chllenge.
+    The goal is to achieve a recall metric of over 0.95.
+
+Data originally from:
+    Forina, M. et al.
+    PARVUS - An Extendible Package for Data Exploration,
+             Classification and Correlation.
+    Institute of Pharmaceutical and Food Analysis and Technologies,
+    Via Brigata Salerno, 16147 Genoa, Italy.
+
+"""
+
+# Standard library imports
+import os
+
+# External imports
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+# Constants
+REGULARIZATION_RATE = 0.01
+
+# load data
+data = pd.read_csv(os.path.join("data", "wine.csv"))
+
+# Extract training features
+features = data.columns.values.tolist()
+
+# Separate WineVariety data from other characteristics
+x, y = data[features].values, data["WineVariety"].values
+
+# Split the data into test and train
+# (setting random_state to see consistent inputs
+#   whilst trying different training techniques)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.30, random_state=0)
+
+# Train LogisticRegression model
+model = LogisticRegression(
+    C=1/REGULARIZATION_RATE, solver="liblinear").fit(x_train, y_train)
+
+# Predict using our verification (test) data
+predictions = model.predict(x_test)
+
+# Compare our predictions to the correct values
+print(f"Prediction: \n{predictions}")
+print(f"Correct: \n{y_test}")
+
+# Print accuracy score, where 1 is 100% accurate and 0 is 0% accurate
+print(f"\nAccuracy score: {round(accuracy_score(y_test, predictions), 3)}")
